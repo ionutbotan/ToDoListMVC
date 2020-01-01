b0ab2561b7578e1ac08ea50c2686ef37baf110a8
ionutbotan <39689490+ionutbotan@users.noreply.github.com>
2020-01-01 12:59:58 +0200

    Implemented Unit testing and Integration testing

---
+++ AspNetCoreTodo.IntegrationTests/TestFixture.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+
+namespace AspNetCoreTodo.IntegrationTests
+{
+    public class TestFixture : IDisposable
+    {
+        private readonly TestServer _server;
+
+        public HttpClient Client { get; }
+
+        public TestFixture()
+        {
+            var builder = new WebHostBuilder()
+                .UseStartup<ToDoListMVC.Startup>()
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.SetBasePath(Path.Combine(
+                        Directory.GetCurrentDirectory(),
+                        "..\\..\\..\\..\\ToDoListMVC"));
+
+                    config.AddJsonFile("appsettings.json");
+                });
+
+            _server = new TestServer(builder);
+
+            Client = _server.CreateClient();
+            Client.BaseAddress = new Uri("http://localhost:8888");
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            _server.Dispose();
+        }
+    }
+}
---
+++ AspNetCoreTodo.UnitTests/TodoItemServiceShould.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoListMVC.Data;
+using ToDoListMVC.Models;
+using ToDoListMVC.Services;
+using Xunit;
+
+namespace AspNetCoreTodo.UnitTests
+{
+    public class TodoItemServiceShould
+    {
+        [Fact]
+        public async Task AddNewItemAsIncompleteWithDueDate()
+        {
+            var options = new DbContextOptionsBuilder<TodoListDbContext>()
+                .UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+            // Set up a context (connection to the "DB") for writing
+            using (var context = new TodoListDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                var fakeUser = new IdentityUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@example.com"
+                };
+
+                await service.AddItemAsync(new TodoItem
+                {
+                    Title = "Testing?"
+                }, fakeUser);
+            }
+
+            // Use a separate context to read data back from the "DB"
+            using (var context = new TodoListDbContext(options))
+            {
+                var itemsInDatabase = await context
+                    .Items.CountAsync();
+                Assert.Equal(1, itemsInDatabase);
+
+                var item = await context.Items.FirstAsync();
+                Assert.Equal("Testing?", item.Title);
+                Assert.False(item.IsDone);
+
+                // Item should be due 3 days from now (give or take a second)
+                var difference = DateTimeOffset.Now.AddDays(3) - item.DueAt;
+                Assert.True(difference < TimeSpan.FromSeconds(1));
+            }
+        }
+
+        [Fact]
+        public async Task ReturnFalseIfWrongIdIsPassedToMarkDoneAsync()
+        {
+            var options = new DbContextOptionsBuilder<TodoListDbContext>()
+                .UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+            // Set up a context (connection to the "DB") for writing
+            using (var context = new TodoListDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                var fakeUser = new IdentityUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@example.com"
+                };
+
+                await service.AddItemAsync(new TodoItem
+                {
+                    Title = "Testing?"
+                }, fakeUser);
+
+                Assert.False(await service.MarkDoneAsync(Guid.NewGuid(), fakeUser));
+            }
+        }
+
+        [Fact]
+        public async Task ReturnTrueIfValidItemIsPassedToMarkDoneAsync()
+        {
+            var options = new DbContextOptionsBuilder<TodoListDbContext>()
+                .UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+            // Set up a context (connection to the "DB") for writing
+            using (var context = new TodoListDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                var fakeUser = new IdentityUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@example.com"
+                };
+
+                await service.AddItemAsync(new TodoItem
+                {
+                    Title = "Testing?"
+                }, fakeUser);
+
+                var items = await service.GetIncompleteItemsAsync(fakeUser);
+                Assert.True(await service.MarkDoneAsync(items[0].Id, fakeUser));
+            }
+        }
+
+        [Fact]
+        public async Task ReturnOnlyItemsOwnedByAParticularUser()
+        {
+            var options = new DbContextOptionsBuilder<TodoListDbContext>()
+                .UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+            // Set up a context (connection to the "DB") for writing
+            using (var context = new TodoListDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                var fakeUser = new IdentityUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@example.com"
+                };
+
+                await service.AddItemAsync(new TodoItem
+                {
+                    Title = "Testing?"
+                }, fakeUser);
+
+                var items = await service.GetIncompleteItemsAsync(fakeUser);
+                Assert.Equal("Testing?", items[0].Title);
+            }
+        }
+    }
+}
